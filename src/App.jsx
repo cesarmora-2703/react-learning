2a0325fa4f91b3e678f552880e9e76841e4e5a5d
Cesar Mora <ingeniotekcol@gmail.com>
2024-06-20 11:19:50 -0500

    Meeting componenets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>The road to react</h1>
+      <input placeholder="Fill the data" />
+      <button
+        onClick={(e) => {
+          console.log(e.target.value);
+        }}
+      >
+        Save
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
