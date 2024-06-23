5fe65ca691d803436279d6823e608d8e378b8f5a
Cesar Mora <ingtkdev@ingeniotek.com.co>
2024-06-23 11:47:07 -0500

    Lists, map function exxercises

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ import "./App.css";
 const sayHello = {
   greet: "Hello world",
   title: "React",
-}
+};
 
-function getText(text){
+const list1 = [
+  {
+    title: "React",
+    url: "https://react.js.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function getText(text) {
   return text;
 }
 
@@ -15,15 +34,40 @@ function App() {
 
   return (
     <>
-      <h1>{getText(sayHello.greet)} The road to {sayHello.title}</h1>
-      <input placeholder="Fill the data" />
-      <button
-        onClick={(e) => {
-          console.log(e.target.value);
-        }}
-      >
-        Save data
-      </button>
+      <h1>
+        {getText(sayHello.greet)} The road to {sayHello.title}
+      </h1>
+      <h1>My hacker Stories</h1>
+      <label htmlFor="Search">Search: </label>
+      <input id="search" type="text" placeholder="Fill the data" />
+      <hr />
+      <div>
+        <ul>
+          {list1.map(function (item) {
+            return (
+              <>
+                <li key={item.objectID}>
+                  <span>
+                    <a href={item.url}>{item.title}</a>
+                  </span>
+                  <span> {item.author} </span>
+                  <span> {item.num_comments} </span>
+                  <span> {item.points} </span>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+      <div>
+        <button
+          onClick={(e) => {
+            console.log(e.target.value);
+          }}
+        >
+          Save data
+        </button>
+      </div>
     </>
   );
 }
