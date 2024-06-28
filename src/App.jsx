19a7e0c587f6871c499b3ead003faadf2f74a70b
Cesar Mora <ingeniotekcol@gmail.com>
2024-06-28 17:36:12 -0500

    Nested destructuring for Item component.

--- src/App.jsx
+++ src/App.jsx
@@ -65,16 +65,14 @@ const List = ({ list }) => (
   </ul>
 );
 
-const Item = ({ item }) => {
-  //with implicit return
-  return (
-    <li>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span> {item.author} </span>
-      <span> {item.num_comments} </span>
-      <span> {item.points} </span>
-    </li>
-  );
-};
+const Item = ({ item: { url, title, author, num_comments, points } }) => (
+  //nested deconstruction
+  <li>
+    <span>
+      <a href={url}> {title}</a>
+    </span>
+    <span> {author} </span>
+    <span> {num_comments} </span>
+    <span> {points} </span>
+  </li>
+);
